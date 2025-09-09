2b9f43f3044e070522fe1c12a5af9fc30ea1a193
Charmi <cparmar01@gmail.com>
2025-09-09 20:13:13 +0530

    Changes

--- server/app.ts
+++ server/app.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend URL
+  origin: "*", // your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
   credentials: true
 }));
